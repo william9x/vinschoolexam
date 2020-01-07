b531354fb764f91b341320f1405bffa69908b214
dungtuan2497 <a0king199x@gmail.com>
2020-01-07 10:38:16 +0700

    fix deployment 2

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -11,24 +11,24 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-//                .paths(PathSelectors.regex("/.*"))
-//                .build()
-//                .apiInfo(apiEndPointsInfo());
-//    }
-//
-//    private ApiInfo apiEndPointsInfo() {
-//        return new ApiInfoBuilder()
-//                .title("User Management REST API")
-//                .description("User Management REST API")
-//                .contact(new Contact("Nguyễn Dũng Tuấn", "", ""))
-//                .version("1.0.0")
-//                .build();
-//    }
-//}
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder()
+                .title("User Management REST API")
+                .description("User Management REST API")
+                .contact(new Contact("Nguyễn Dũng Tuấn", "", ""))
+                .version("1.0.0")
+                .build();
+    }
+}
